655d949eb229dab2db0c41f8d304e163dae59e3c
VTaPo <105722903+VTaPo@users.noreply.github.com>
2023-03-25 10:29:51 +0700

    calculate_2numeric_columns.py last version

--- Lab01/SourceCode/calculate_2numeric_columns.py
+++ Lab01/SourceCode/calculate_2numeric_columns.py
@@ -19,11 +19,13 @@ long_options=["help", "input=","first=","second=","method=","output="]
 try:
 	arguments, values = getopt.getopt(argumentList, options, long_options)
 
-	if arguments[0][0] in ("-h", "--help"):
+	if len(arguments)==1 and arguments[0][0] in ("-h", "--help"):
 		helpMessageForCalculateFunctions()
 	else:
 		for i in range(len(arguments)):
-			if arguments[i][0] in ("-i", "--input"):
+			if arguments[0][0] in ("-h", "--help"):
+				helpMessageForCalculateFunctions()
+			elif arguments[i][0] in ("-i", "--input"):
 				input_=str(arguments[i][1])
 			elif arguments[i][0] in ("-m", "--method"):
 				method = arguments[i][1]
